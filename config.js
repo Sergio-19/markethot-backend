19f16da8b8fbc7181709c8bbf67ec64c36f31638
unknown <you@example.com>
2022-11-24 17:19:56 +0300

    исправление конфиг файла

--- config.js
+++ config.js
@@ -10,7 +10,9 @@ const config = {
             host: 'stended3.beget.tech',
             user: 'stended3_rudemy',
             password: 'Se549297',
-            database: 'stended3_rudemy'
+            database: 'stended3_rudemy',
+            shopid: '755574',
+            apikey: 'live_3ysrwvu7WUK_Mq_FZ15MbFLcTshxxR81jEhFYKBkv70'
 
 }
 
